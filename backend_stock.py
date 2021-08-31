ea968081979cb19c2b227a8eb8064d4b7aa0c119
Brendan Lucich <brendanlucich@gmail.com>
2021-08-30 22:12:53 -0500

    Update backend_stock.py
    Parse API for most recent day close stock price

--- backend_stock.py
+++ backend_stock.py
@@ -1,7 +1,10 @@
 # Backend Stock Processing
 
-# comparing intrinsic value vs the actual value 
+import requests
+import json
 
+
+# comparing intrinsic value vs the actual value 
 
 # CORNERSTONES
 # pe ratio
@@ -20,3 +23,39 @@
 
 # NLP for stock analysts and back testing 
 
+def set_api_urls(api_urls, ticker):
+    api_urls["daily_adjusted_url"] = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=' + ticker + '&apikey=demo'
+
+# get most recent stock price at close
+def get_stock_price(ticker):
+    req = requests.get(api_urls["daily_adjusted_url"])
+    response = req.json()
+    day_data = response["Time Series (Daily)"]
+    latest_day = next(iter(day_data))
+    latest_close_price = day_data[latest_day]["4. close"]
+    print(latest_close_price)
+
+
+def call_evaluations(ticker):
+    print("call evaluations on " + ticker)
+    get_stock_price(ticker)
+
+# start
+print("Backend Stock Processing\n")
+
+# get ticker from user, no error checking yet
+ticker = input("Please enter a stock ticker: ")
+ticker = str(ticker)
+ticker = ticker.upper()
+print(ticker)
+
+# set api urls 
+api_urls = {}
+set_api_urls(api_urls, ticker)
+# print(api_urls)
+
+# call evaluations
+call_evaluations(ticker)
+
+# end
+print("\nBackend Stock Processed ")
