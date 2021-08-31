b9a76cb06fb9c59a694a5c20865adb7b1b269d39
Brendan Lucich <brendanlucich@gmail.com>
2021-08-31 09:44:00 -0500

    Backend PE and Profit Margin
    Able to get PE Ratio and profit margin from the API requests

--- backend_stock.py
+++ backend_stock.py
@@ -8,27 +8,6 @@ def set_api_urls(api_urls, ticker, key):
     api_urls["intraday_url"]=  'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + ticker + '&interval=1min&apikey=' + key
     api_urls["company_overview_url"] = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol=' + ticker + '&apikey=' + key
 
-# get most recent stock price at close
-def get_stock_price(ticker):
-    req = requests.get(api_urls["intraday_url"])
-    response = req.json()
-    # print(response)
-    minute_data = response["Time Series (1min)"]
-    latest_minute = next(iter(minute_data))
-    latest_close_price = minute_data[latest_minute]["4. close"]
-    print("The stock price of " + ticker + " is " + latest_close_price)
-    return latest_close_price
-
-# def get_pe():
-# def get_profit_margin():
-# def get_profit_growth():
-# def get_revenue_growth():
-# def get_current_assets_vs_liabilities():
-# def get_shares_outstanding():
-# def get_free_cash_flow_growth():
-# def get_free_cash_flow_evaluation():
-
-
 def get_company_overview(ticker):
     req = requests.get(api_urls["company_overview_url"])
     response = req.json()
@@ -52,16 +31,55 @@ def get_company_overview(ticker):
         'dividend_date' : response["DividendDate"],
         'exdividend_data' : response["ExDividendDate"]
     }
-    for key in overview_dict:
-        print(key)
-        print(overview_dict[key])
+    # for key in overview_dict:
+    #     print(key)
+    #     print(overview_dict[key])
     return overview_dict
 
+# get most recent stock price at close as a STRING
+def get_stock_price(ticker):
+    req = requests.get(api_urls["intraday_url"])
+    response = req.json()
+    # print(response)
+    minute_data = response["Time Series (1min)"]
+    latest_minute = next(iter(minute_data))
+    latest_close_price = minute_data[latest_minute]["4. close"]
+    # print("The stock price of " + ticker + " is " + latest_close_price)
+    return latest_close_price
 
-def call_evaluations(ticker):
+# returns pe ration as a STRING
+def get_pe(company_overview_dict):
+    pe_ratio = company_overview_dict["pe_ratio"]
+    return pe_ratio
+
+# returns profit margin as a percent as a FLOAT
+def get_profit_margin(company_overview_dict):
+    profit_margin = company_overview_dict["profit_margin"]
+    # print(profit_margin)
+    profit_margin = float(profit_margin) * 100
+    return profit_margin
+
+
+# def get_profit_growth():
+# def get_revenue_growth():
+# def get_current_assets_vs_liabilities():
+# def get_shares_outstanding():
+# def get_free_cash_flow_growth():
+# def get_free_cash_flow_evaluation():
+
+
+def evaluation_processing(ticker):
     # print("call evaluations on " + ticker)
-    company_overciew_dict = get_company_overview(ticker)
+    company_overview_dict = get_company_overview(ticker)
     stock_price = get_stock_price(ticker)
+    print("Stock price: " + stock_price)
+    pe_ratio = get_pe(company_overview_dict)
+    print("PE: " + pe_ratio)
+    profit_margin = get_profit_margin(company_overview_dict)
+    print("Profit Margin: " + str(profit_margin))
+
+def db_postions_processing():
+    print("To be developed with Brady")
 
 # start
 print("Backend Stock Processing\n")
@@ -79,7 +97,7 @@ set_api_urls(api_urls, ticker, key)
 # print(api_urls)
 
 # call evaluations
-call_evaluations(ticker)
+evaluation_processing(ticker)
 
 # end
 print("\nBackend Stock Processed ")
