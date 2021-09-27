238083d3773f545b695c674589ecbbbcf1afe72c
BradyMack <50088297+BradyMack@users.noreply.github.com>
2021-09-27 18:35:23 -0500

    Added flask api support

--- backend_api/app.py
+++ backend_api/app.py
@@ -1,7 +1,12 @@
+from backend_processing.db_wrapper import edit_positions
 from flask import Flask
+from flask.wrappers import Request
+from werkzeug.wrappers import request
+from flask import request as rq
 from flask_cors import CORS
 import time
 from backend_processing.backend_stock import my_main
+import backend_processing.db_wrapper as db
 
 
 app = Flask(__name__)
@@ -18,14 +23,59 @@ def get_stock_evaluation(ticker):
     # }
     return response
 
-@app.route('/personal-portfolio')
-def get_personal_portfolio():
-    print("GET")
-    response = {'req': "you want a personal portfolio, req should be from frontend"}
-    return response
+@app.route('/personal-portfolio/<username>', methods=['GET', 'PUT', 'DELETE', 'POST'])
+def get_personal_portfolio(username):
+    if rq.method == 'GET':
+        print('GET')
+        try:
+            user_response = db.get_positions(username)
+            response = {username: user_response}
+            return response
+        except:
+            return 'Could not perform GET function\nEnsure the username entered is valid'
+    if rq.method == 'PUT':
+        try:
+            ticker = rq.form['ticker']
+            avg_price = rq.form['avg_price']
+            qty = rq.form['qty']
+            if db.edit_positions(username, ticker, avg_price, qty) == 1:
+                return ticker +  ' Updated'
+            else:
+                return 'Could not edit ' + ticker
+        except:
+            return 'Could not perform the PUT function\n Ensure the PUT body includes ticker, avg_price, and qty'
+    if rq.method == 'POST':
+        print('POST')
+        try:
+            ticker = rq.form['ticker']
+            avg_price = rq.form['avg_price']
+            qty = rq.form['qty']
+            db.add_position(username, ticker, avg_price, qty)
+            return 'Added position ' + ticker + ' successfully'
+        except:
+            return 'Could not perform the POST function\nEnsure your POST body is includes ticker, avg_price, and qty'
 
-@app.route('/stock-price')
-def get_stock_price():
+    if rq.method == 'DELETE':
+        try:
+            ticker = rq.form['ticker']
+            if db.edit_positions(username, ticker, 0, 0) == 1:
+                return ticker +  ' Deleted!'
+            else:
+                return 'Could not delete ' + ticker
+        except:
+            return 'Could not perform the DELETE function\n Ensure the DELETE body includes ticker'
+
+    return ''
+
+@app.route('/user/<username>', methods=['GET'])
+def create_user(username):
+    if db.add_user(username) == 1:
+        return 'Added ' + username
+    else:
+        return 'Could not add user'
+
+@app.route('/stock-price/<ticker>', methods=['GET'])
+def get_stock_price(ticker):
     print("GET")
     response = {'req': "you want a stock price, req should be from backend"}
     return response
--- backend_api/backend_processing/db_wrapper.py
+++ backend_api/backend_processing/db_wrapper.py
@@ -11,10 +11,11 @@ PRICE_COLLECTION = DB.get_collection('dev-prices')
 def add_user(username):
     if not USER_COLLECTION.find_one({'username': username}):
         added_document = USER_COLLECTION.insert_one({'username': username, 'positions': []})
+        return 1
     else:
-        print('Error user exists')
+        return 0
 
-    print(USER_COLLECTION.find_one({'username': username}))
+    # print(USER_COLLECTION.find_one({'username': username}))
 
 
 def add_positions(username, positions): # positions format [{'ticker': 'ticker', 'avg_price': 250, 'qty': 10}, ...]
@@ -26,17 +27,19 @@ def add_positions(username, positions): # positions format [{'ticker': 'ticker',
 def add_position(username, ticker, avg_price, qty):
     positions = USER_COLLECTION.find({'username': username}, {'positions': 1})[0]['positions']
     print(positions)
-    positions.append({'ticker': ticker, 'avg_price': avg_price, 'qty': qty})
-    USER_COLLECTION.find_one_and_update({'username': username}, {'$set' : {'positions': positions}})
+    if ticker not in positions:
+        positions.append({'ticker': ticker, 'avg_price': avg_price, 'qty': qty})
+        USER_COLLECTION.find_one_and_update({'username': username}, {'$set' : {'positions': positions}})
+        return 1
+    else:
+        return 0
 
 def get_positions(username):
     return USER_COLLECTION.find({'username': username}, {'positions': 1})[0]['positions']
 
 def edit_positions(username, ticker, avg_price, qty):
     positions = USER_COLLECTION.find({'username': username}, {'positions': 1})[0]['positions']
-    print(positions)
     for pos in range(0, len(positions)):
-        print(pos)
         if positions[pos]['ticker'] == ticker:
             if qty == 0:
                 del positions[pos]
