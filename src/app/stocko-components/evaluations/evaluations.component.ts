6b37af67080ce1b1e9e8fdf5b18a9705895a90dc
Alex Davis <19788352+adavis25@users.noreply.github.com>
2021-10-21 16:53:27 -0500

    update for evaluations

--- src/app/stocko-components/evaluations/evaluations.component.ts
+++ src/app/stocko-components/evaluations/evaluations.component.ts
@@ -14,19 +14,21 @@ export class EvaluationsComponent implements OnInit {
   bad = "bg-danger";
   error = "bg-info";
 
-  peRatio = null;                       
-  profitMargin = null;                  
-  profitGrowth = null;                  
-  revenueGrowth = null;                 
-  currentAssetsVsLiabilities = null;    
-  totalAssetsVsLiabilities= null;       
-  changeInSharesOutstanding= null;      
-  freeCashFlowGrowth= null;             
-  freeCashFlowDesiredSharePrice= null;  
-  freeCashFlowDesiredMarketCap = null;  
-  evEbitdaRatio = null;                 
-  priceToBookRatio = null;   
-  stockPrice = null; 
+  peRatio: number = 0;                       
+  profitMargin: number = 0;                  
+  profitGrowth: number = 0;                  
+  revenueGrowth: number = 0;                 
+  currentAssetsVsLiabilities: number = 0;    
+  totalAssetsVsLiabilities: number= 0;       
+  changeInSharesOutstanding: number= 0;      
+  freeCashFlowGrowth: number= 0;             
+  freeCashFlowDesiredSharePrice: number= 0;  
+  freeCashFlowDesiredMarketCap: number = 0;  
+  evEbitdaRatio: number = 0;                 
+  priceToBookRatio: number = 0;   
+  stockPrice: number = 0; 
+  marketCap: number = 0; 
+
 
   peRatioS = "";                       
   profitMarginS = "";                  
@@ -65,43 +67,48 @@ export class EvaluationsComponent implements OnInit {
       .subscribe(data => { 
         console.log(data)
         this.stockPrice = data.stock_price;
+        this.marketCap = data.market_cap;
         this.historyError = data.years_of_history_error;
 
         this.peRatio = data.pe_ratio;
-        this.peRatioS = this.checkOneReverse(data.pe_ratio, 20)
+        
+        this.peRatioS = this.checkOneReverse(data.pe_ratio, 20);
 
         this.profitMargin = data.profit_margin;
-        this.profitMarginS = this.checkOne(data.profit_margin, 10)
+        this.profitMarginS = this.checkOne(data.profit_margin, 10);
 
         this.profitGrowth = data.profit_growth;
-        this.profitGrowthS = this.checkOne(data.profit_growth, 0)
+        this.profitGrowthS = this.checkOne(data.profit_growth, 0);
 
         this.revenueGrowth = data.revenue_growth;
-        this.revenueGrowthS = this.checkOne(data.revenue_growth, 0)
+        this.revenueGrowthS = this.checkOne(data.revenue_growth, 0);
 
         this.currentAssetsVsLiabilities = data.current_assets_vs_liabilities;
-        this.currentAssetsVsLiabilitiesS = this.checkOne(data.current_assets_vs_liabilities, 0)
+        this.currentAssetsVsLiabilitiesS = this.checkOne(data.current_assets_vs_liabilities, 0);
 
         this.totalAssetsVsLiabilities = data.total_assets_vs_liabilities;
-        this.totalAssetsVsLiabilitiesS = this.checkOne(data.total_assets_vs_liabilities, 0)
+        this.totalAssetsVsLiabilitiesS = this.checkOne(data.total_assets_vs_liabilities, 0);
 
         this.changeInSharesOutstanding = data.change_in_shares_ourstanding;
-        this.changeInSharesOutstandingS = this.checkOne(data.change_in_shares_ourstanding, 0)
+        this.changeInSharesOutstandingS = this.checkOneReverse(data.change_in_shares_ourstanding, 0);
 
         this.freeCashFlowGrowth = data.fcf_growth;
-        this.freeCashFlowGrowthS = this.checkOne(data.fcf_growth, 0)
+        this.freeCashFlowGrowthS = this.checkOne(data.fcf_growth, 0);
 
         this.freeCashFlowDesiredSharePrice = data.fcf_desired_price;
-        this.freeCashFlowDesiredSharePriceS = this.checkOne(data.fcf_desired_price, data.stock_price)
+        this.freeCashFlowDesiredSharePriceS = this.checkOneReverse(data.fcf_desired_price, data.stock_price);
 
         this.freeCashFlowDesiredMarketCap = data.fcf_desired_marketcap;
-        this.freeCashFlowDesiredMarketCapS = this.checkOne(data.fcf_desired_marketcap, 20)
+        console.log(data.fcf_desired_marketcap)
+        console.log(data.market_cap)
+
+        this.freeCashFlowDesiredMarketCapS = this.checkOne(data.fcf_desired_marketcap, data.market_cap);
 
         this.evEbitdaRatio = data.ev_ebitda_ratio;
         this.evEbitdaRatioS = this.checkOneReverse(data.ev_ebitda_ratio, 10)
 
         this.priceToBookRatio = data.price_to_book_ratio;
-        this.priceToBookRatioS = this.checkOne(data.price_to_book_ratio, 20)
+        this.priceToBookRatioS = this.checkOneReverse(data.price_to_book_ratio, 20)
 
 
       })
