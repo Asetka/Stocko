0bcfe65da231c858cf1506056e9084db101b5908
Alex Davis <19788352+adavis25@users.noreply.github.com>
2021-10-24 22:48:47 -0500

    Changed UI

--- src/app/stocko-components/positions/positions.component.ts
+++ src/app/stocko-components/positions/positions.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { catchError, findIndex, map, tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-positions',
@@ -10,7 +9,11 @@ import { catchError, findIndex, map, tap } from 'rxjs/operators';
   styleUrls: ['./positions.component.sass']
 })
 export class PositionsComponent implements OnInit {
-  base = `https://stocko-flask-api-dev.herokuapp.com`
+
+  portfolioGain: number = 0;
+  portfolioProfit: number = 0;
+
+  base = `https://stocko-flask-api-dev.herokuapp.com/personal-portfolio`
   priceHelper : any;
   d : any;
   userName : string | undefined;
@@ -34,27 +37,22 @@ export class PositionsComponent implements OnInit {
       this.getPositions(data?.nickname).subscribe(data => this.d = data)
       this.userName = data?.nickname;
     })
+
   }
 
   getPositions(uid: string | undefined) {
-    const url = `${this.base}/personal-portfolio/${uid}`;
+    const url = `${this.base}/${uid}`;
     return this.http.get<any[]>(url)
       .pipe(tap(x => {
         this.d = x;
         this.myArr = this.d.response;
+        this.portfolioProfit = this.getPortfolioSum(this.myArr);
+        this.portfolioGain = this.getTotalPercentChange(this.myArr);
+
       }))
   }
-
-  getPrice(ticker: string){
-    const url = `${this.base}/stock-price/${ticker}`
-    this.http.get<any>(url).subscribe(data => {
-      console.log(data.price)
-      return data.price;
-    })
-  }
-
   addPosition(ticker: string, avgPrice: number, quantity: number){
-    const url = `${this.base}/personal-portfolio/${this.userName}`;
+    const url = `${this.base}/${this.userName}`;
     const formData = new FormData()
     formData.set("ticker", ticker.toUpperCase())
     formData.set("avg_price", avgPrice.toString());
@@ -64,7 +62,7 @@ export class PositionsComponent implements OnInit {
   }
 
   editPosition(ticker: string, avgPrice: number, quantity: number){
-    const url = `${this.base}/personal-portfolio/${this.userName}`;
+    const url = `${this.base}/${this.userName}`;
     const formData = new FormData()
     formData.set("ticker", ticker.toUpperCase())
     formData.set("avg_price", avgPrice.toString());
@@ -74,7 +72,7 @@ export class PositionsComponent implements OnInit {
   }
 
   deletePosition(val: Position){
-    const url = `${this.base}/personal-portfolio/${this.userName}`;
+    const url = `${this.base}/${this.userName}`;
     console.log(this.userName);
     console.log(url)
     const formData = new FormData()
@@ -82,7 +80,22 @@ export class PositionsComponent implements OnInit {
     this.http.delete<any>(url, {body: formData}).subscribe();
     this.myArr.splice(this.myArr.indexOf(val),1);
   }
+  
+  getPortfolioSum(arr: Position[]): number{
+    arr.forEach(element => {
+      this.portfolioProfit = this.portfolioProfit + element.profit;
+    });
+    return this.portfolioProfit;
+  }
+
+  getTotalPercentChange(arr: Position[]): number{
+    arr.forEach(element => {
+      this.portfolioGain = this.portfolioGain + element.pct_change;
+    });
+    return this.portfolioGain;
+  }
 }
+
 
 export interface Position {
   avg_price : number;
