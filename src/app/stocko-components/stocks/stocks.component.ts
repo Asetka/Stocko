b65a451230ce80e9e7bb3be70424d866a6015a82
Alex Davis <19788352+adavis25@users.noreply.github.com>
2021-10-26 00:40:39 -0500

    Chart WOrks

--- src/app/stocko-components/stocks/stocks.component.ts
+++ src/app/stocko-components/stocks/stocks.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import * as Highcharts from 'highcharts';
 @Component({
@@ -6,33 +7,66 @@ import * as Highcharts from 'highcharts';
   styleUrls: ['./stocks.component.sass']
 })
 export class StocksComponent implements OnInit {
+  ticker: string = "";
+  stockObject: any;
+  searched: boolean = false;
+  stockPrices: number[] = []
   highcharts = Highcharts;
   chartOptions: Highcharts.Options = {};
-  constructor() { }
 
+  constructor(
+    private http: HttpClient,
+  ) { }
+  
   ngOnInit(): void {
-    this.highcharts = Highcharts;
-    this.chartOptions = {
-      title: {
-        text: "Apple"
-      },
-      xAxis: {
-        title: {
-          text: 'Date'
-        },
-        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-          "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-      },
-      yAxis: {
-        title: {
-          text: "Price"
-        }
-      },
-      series: [{
-        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 24.4, 19.3, 16.0, 18.4, 17.9],
-        type: 'spline'
-      }]
-    }
-  }
 
+  }
+  onSearch(){
+    const base = `https://stocko-flask-api-dev.herokuapp.com/stock-page/`
+    const url = `${base}/${this.ticker}`;
+    this.http.get<any>(url).subscribe(data => {
+      console.log(data)
+      this.stockObject = JSON.parse(JSON.stringify(data))
+      if(this.stockObject != null){
+        this.searched = true;
+      }
+      console.log(this.stockObject.chart_data.week_ending)
+      const stockData = this.stockObject.chart_data.end_of_week_price;
+      const stockDates = this.stockObject.chart_data.week_ending;
+
+      console.log(stockData)
+
+      var stockPrices = new Array;
+      var stockPrices = new Array;
+
+      for(var i in stockData){
+        console.log(stockData[i]);
+        stockPrices.push(Number(stockData[i]))
+      }
+      console.log(stockPrices)
+      this.highcharts = Highcharts;
+      this.chartOptions = {
+        title: {
+          text: this.stockObject.ticker
+        },
+        xAxis: {
+          title: {
+            text: 'Date'
+          },
+          categories: this.stockObject.chart_data.week_ending
+        },
+        yAxis: {
+          title: {
+            text: "Price"
+          }
+        },
+        series: [{
+          data: stockPrices,
+          type: 'spline'
+        }]
+      }
+    })
+    console.log(this.searched)
+
+  }
 }
