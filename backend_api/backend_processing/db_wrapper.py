068c32fab757d0884a4923995a84fc9df531a463
BradyMack <50088297+BradyMack@users.noreply.github.com>
2021-11-22 18:42:03 -0600

    adding console print

--- backend_api/backend_processing/db_wrapper.py
+++ backend_api/backend_processing/db_wrapper.py
@@ -53,6 +53,7 @@ def get_positions(username):
         qty = positions[position_index]['qty']
         avg_price = positions[position_index]['avg_price']
         try:
+            print(price)
             price = float(get_price(positions[position_index]['ticker']))
             positions[position_index]['price'] = price
             positions[position_index]['profit'] = (float(qty)*price)-float(avg_price)*float(qty)
