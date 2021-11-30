865aa8f6d41134bdebee36fbc2f2f8d74f3a7e3f
BradyMack <50088297+BradyMack@users.noreply.github.com>
2021-11-29 22:39:44 -0600

    Revert "Merge pull request #80 from Asetka/tc3"
    This reverts commit bb826609ed8bd8382d33eebdc865f4dfc9a5f64e, reversing
    changes made to bf9ef8dad951edb2f5042a77cf65c00409ca0fcf.

--- backend_api/backend_processing/stock_price.py
+++ backend_api/backend_processing/stock_price.py
@@ -6,7 +6,7 @@ def get_stock_price(ticker, api_urls):
 
     req = requests.get(api_urls["intraday_url"])
     response = req.json()
-    minute_data = response["Time Series (5min)"]
+    minute_data = response["Time Series (1min)"]
     latest_minute = next(iter(minute_data))
     latest_close_price = minute_data[latest_minute]["4. close"]
     latest_close_price = float(latest_close_price)
